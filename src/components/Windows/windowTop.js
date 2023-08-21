1bf4174ac6ff25820734d240054a12418a8bf7ba
William Li <52115161+Trollermaner@users.noreply.github.com>
2023-08-21 18:01:25 -0400

    finished the about me section

--- src/components/Windows/windowTop.js
+++ src/components/Windows/windowTop.js
@@ -4,13 +4,13 @@ export default function WindowTop(props) {
     const [hover, setHover] = useState(false)
 
     return(
-        <div id = "topWindow" className="flex flex-row justify-between hover:cursor-grab select-none" style={{height: "30px", padding: "2.5px", paddingLeft: "15px", paddingRight: 0}}>{/** top window part with buttons and shit */}
+        <div id = "topWindow" className="flex flex-row justify-between hover:cursor-grab select-none topWindow" style={{height: "30px", padding: "2.5px", paddingLeft: "15px", paddingRight: 0}}>{/** top window part with buttons and shit */}
             <div className="text-white font-semibold" style={{zIndex: 1}}>{props.title}</div>
             <div onMouseEnter={() => {setHover(true)}} onMouseLeave={() => {setHover(false)}} className="flex justify-center text-white hover:cursor-pointer" style={{zIndex: 2, width: "30px"}}>{/**triple buttons */}
                 X
             </div>
-            <div className="absolute hover:cursor-pointer" style={{backgroundColor: props.clicked || hover? "#DC143C" : "grey", height: "30px", width: "30px", marginTop: "-2.5px", opacity: "70%", marginLeft: "calc(" + props.width + " - 49px)", zIndex: 1, borderBottomWidth: "2px"}}></div>
-            <div className={"absolute"} style={{backgroundColor: props.clicked? "#006CFF" : "black", height: "30px", width: "calc(" + props.width + " - 34px)", marginLeft: "-15px", marginTop: "-2.5px", opacity: "70%", borderBottomWidth: "2px"}}></div>
+            <div className="absolute hover:cursor-pointer" style={{backgroundColor: props.clicked || hover? "#DC143C" : "grey", height: "30px", width: "30px", marginTop: "-2.5px", opacity: props.clicked? "1": "70%", marginLeft: "calc(" + props.width + " - 49px)", zIndex: 1, borderBottomWidth: "2px"}}></div>
+            <div className={"absolute"} style={{backgroundColor: props.clicked? "#006CFF" : "black", height: "30px", width: "calc(" + props.width + " - 34px)", marginLeft: "-15px", marginTop: "-2.5px", opacity:props.clicked? "1" : "70%", borderBottomWidth: "2px"}}></div>
         </div> 
     )
 }
